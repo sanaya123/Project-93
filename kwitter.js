925b2812283313ee286d6f3f53e6cbed93f4b133
sanaya123 <83179671+sanaya123@users.noreply.github.com>
2022-01-16 22:51:26 -0600

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,5 +1,5 @@
 function login(){
-    User_name = document.getElementById("username").value
-    localStorage.setItem("user_name", User_name)
+    User_name = document.getElementById("user_name").value
+    localStorage.setItem("username", User_name)
     window.location="kwitter_room.html"
 }
